8388efa43cd5acda47914e64fd345a5d1631ee30
zhzhao <zhzhao999@126.com>
2018-03-09 17:29:37 +0800

    first commit

---
+++ src/main/java/zhzhao/top/Demo.java
@@ -0,0 +1,15 @@
+/**
+ * https://www.zhzhao.top
+ */
+package zhzhao.top;
+
+/**
+ *
+ *@author zhzhao
+ *@version $ Id: Demo.java,V 0.1 2018/3/9 17:16 zhzhao Exp $
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
